e61e2a00326633f1438662b92d155dfa8cf4eaf0
fatemaraidah <yushahasan98@gmail.com>
2021-07-27 09:49:04 +0600

    First commit

---
+++ src/Array.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Array
+{
+    public static void main (String[] args )
+    {
+        Scanner s= new Scanner(System.in);
+        System.out.print("Enter the length of the array: ");
+        int length = s.nextInt();
+        int myArray[] = new int[length];
+        System.out.println("Enter the elements: ");
+        
+        for(int i=0; i<length; i++)
+        {
+            myArray[i]= s.nextInt();
+             sum = sum + myArray[i]
+        }
+         System.out.println(Arrays.toString(myArray));
+    }
+   
+}       
